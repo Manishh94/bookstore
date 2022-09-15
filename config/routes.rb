3d7d3e7d082b2a41849ff3e739e6ba952961f328
Manish Prajapat <manishprajapat94@gmail.com>
2022-09-15 15:51:21 +0530

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :courses
+  resources :courses do
+    member do
+      get :students
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users
+
+  root to: 'users#index'
 end
